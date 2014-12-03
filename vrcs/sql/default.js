ecc2e69496b9bbb0b6fbf49c0fdc720ac98e9328
pi <tjdrms0121@gmail.com>
2014-12-04 00:28:13 +0900

    4명이상 접근할경우 방에 못들거도록 설정

--- vrcs/sql/default.js
+++ vrcs/sql/default.js
@@ -21,20 +21,28 @@ db.serialize(function() {
 
     db.get(sql.user.select, function(err, uData) {
         console.log(uData);
-        var volume = {
+        var volume = [{
             $userpn : uData.pn,
             $title : 'Volume Rendering',
             $saveName : '[2014-11-27 20:00:00]_head.dan',
             $fileName : 'head.dan'
-        };
+        },{
+            $userpn : uData.pn,
+            $title : 'Volume Rendering2',
+            $saveName : '[2014-11-27 20:00:00]_head2.dan',
+            $fileName : 'head2.dan'
+        }];
 
         stmt = db.prepare(sql.volume.insert);
-        stmt.run(volume);
-        stmt.finalize();
-
-        db.each(sql.volume.selectVolumeList, { $userpn : uData.pn }, function(err, vData){
-            console.log(vData)
+        stmt.run(volume[0], function(){
+            stmt = db.prepare(sql.volume.insert);
+            stmt.run(volume[1], function(){
+                db.each(sql.volume.selectVolumeList, { $userpn : uData.pn }, function(err, vData){
+                    console.log(vData);
+                });
+            });
         });
+        stmt.finalize();
     });
 
 });
