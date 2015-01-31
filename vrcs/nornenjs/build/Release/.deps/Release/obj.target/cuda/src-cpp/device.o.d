07f3e1ac1b19c05511a3f27aa8c94cbbb59a496b
Francis0121 <tjdrms0121@gmail.com>
2015-01-31 20:50:19 +0900

    .data 로 확장자 변경

---
+++ vrcs/nornenjs/build/Release/.deps/Release/obj.target/cuda/src-cpp/device.o.d
@@ -0,0 +1,25 @@
+cmd_Release/obj.target/cuda/src-cpp/device.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/pi/.node-gyp/0.10.35/src -I/home/pi/.node-gyp/0.10.35/deps/uv/include -I/home/pi/.node-gyp/0.10.35/deps/v8/include -I/usr/local/include -I/usr/local/cuda-6.0/include -I/usr/local/cuda/include  -Wall -Wextra -Wno-unused-parameter -pthread -m32 -O2 -fno-strict-aliasing -fno-tree-vrp -fno-omit-frame-pointer -fno-rtti -fno-exceptions -MMD -MF ./Release/.deps/Release/obj.target/cuda/src-cpp/device.o.d.raw  -c -o Release/obj.target/cuda/src-cpp/device.o ../src-cpp/device.cpp
+Release/obj.target/cuda/src-cpp/device.o: ../src-cpp/device.cpp \
+ ../src-cpp/device.hpp /usr/local/cuda-6.0/include/cuda.h \
+ ../src-cpp/bindings.hpp /home/pi/.node-gyp/0.10.35/deps/v8/include/v8.h \
+ /home/pi/.node-gyp/0.10.35/deps/v8/include/v8stdint.h \
+ /home/pi/.node-gyp/0.10.35/src/node.h \
+ /home/pi/.node-gyp/0.10.35/deps/uv/include/uv.h \
+ /home/pi/.node-gyp/0.10.35/deps/uv/include/uv-private/uv-unix.h \
+ /home/pi/.node-gyp/0.10.35/deps/uv/include/uv-private/ngx-queue.h \
+ /home/pi/.node-gyp/0.10.35/deps/uv/include/uv-private/uv-linux.h \
+ /home/pi/.node-gyp/0.10.35/src/node_object_wrap.h \
+ /home/pi/.node-gyp/0.10.35/src/node.h
+../src-cpp/device.cpp:
+../src-cpp/device.hpp:
+/usr/local/cuda-6.0/include/cuda.h:
+../src-cpp/bindings.hpp:
+/home/pi/.node-gyp/0.10.35/deps/v8/include/v8.h:
+/home/pi/.node-gyp/0.10.35/deps/v8/include/v8stdint.h:
+/home/pi/.node-gyp/0.10.35/src/node.h:
+/home/pi/.node-gyp/0.10.35/deps/uv/include/uv.h:
+/home/pi/.node-gyp/0.10.35/deps/uv/include/uv-private/uv-unix.h:
+/home/pi/.node-gyp/0.10.35/deps/uv/include/uv-private/ngx-queue.h:
+/home/pi/.node-gyp/0.10.35/deps/uv/include/uv-private/uv-linux.h:
+/home/pi/.node-gyp/0.10.35/src/node_object_wrap.h:
+/home/pi/.node-gyp/0.10.35/src/node.h:
---
+++ vrcs/nornenjs/build/Release/.deps/Release/obj.target/cuda/src-cpp/function.o.d
@@ -0,0 +1,30 @@
+cmd_Release/obj.target/cuda/src-cpp/function.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/pi/.node-gyp/0.10.35/src -I/home/pi/.node-gyp/0.10.35/deps/uv/include -I/home/pi/.node-gyp/0.10.35/deps/v8/include -I/usr/local/include -I/usr/local/cuda-6.0/include -I/usr/local/cuda/include  -Wall -Wextra -Wno-unused-parameter -pthread -m32 -O2 -fno-strict-aliasing -fno-tree-vrp -fno-omit-frame-pointer -fno-rtti -fno-exceptions -MMD -MF ./Release/.deps/Release/obj.target/cuda/src-cpp/function.o.d.raw  -c -o Release/obj.target/cuda/src-cpp/function.o ../src-cpp/function.cpp
+Release/obj.target/cuda/src-cpp/function.o: ../src-cpp/function.cpp \
+ /home/pi/.node-gyp/0.10.35/src/node_buffer.h \
+ /home/pi/.node-gyp/0.10.35/src/node.h \
+ /home/pi/.node-gyp/0.10.35/deps/uv/include/uv.h \
+ /home/pi/.node-gyp/0.10.35/deps/uv/include/uv-private/uv-unix.h \
+ /home/pi/.node-gyp/0.10.35/deps/uv/include/uv-private/ngx-queue.h \
+ /home/pi/.node-gyp/0.10.35/deps/uv/include/uv-private/uv-linux.h \
+ /home/pi/.node-gyp/0.10.35/deps/v8/include/v8.h \
+ /home/pi/.node-gyp/0.10.35/deps/v8/include/v8stdint.h \
+ /home/pi/.node-gyp/0.10.35/src/node_object_wrap.h \
+ ../src-cpp/function.hpp /usr/local/cuda-6.0/include/cuda.h \
+ ../src-cpp/bindings.hpp /home/pi/.node-gyp/0.10.35/src/node.h \
+ ../src-cpp/module.hpp ../src-cpp/mem.hpp
+../src-cpp/function.cpp:
+/home/pi/.node-gyp/0.10.35/src/node_buffer.h:
+/home/pi/.node-gyp/0.10.35/src/node.h:
+/home/pi/.node-gyp/0.10.35/deps/uv/include/uv.h:
+/home/pi/.node-gyp/0.10.35/deps/uv/include/uv-private/uv-unix.h:
+/home/pi/.node-gyp/0.10.35/deps/uv/include/uv-private/ngx-queue.h:
+/home/pi/.node-gyp/0.10.35/deps/uv/include/uv-private/uv-linux.h:
+/home/pi/.node-gyp/0.10.35/deps/v8/include/v8.h:
+/home/pi/.node-gyp/0.10.35/deps/v8/include/v8stdint.h:
+/home/pi/.node-gyp/0.10.35/src/node_object_wrap.h:
+../src-cpp/function.hpp:
+/usr/local/cuda-6.0/include/cuda.h:
+../src-cpp/bindings.hpp:
+/home/pi/.node-gyp/0.10.35/src/node.h:
+../src-cpp/module.hpp:
+../src-cpp/mem.hpp:
